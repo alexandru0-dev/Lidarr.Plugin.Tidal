c962aab007cc6e5a41f2c35386b9d39cc9eeb699
Alexandru Nechita <alexandru.italia32@gmail.com>
2024-12-05 19:42:13 +0100

    chore: setup nix and direnv

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ dotnet-sdk_8 ];
+          };
+        };
+    };
+}
